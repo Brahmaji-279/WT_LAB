19a5ebd33b3236fae28278116248afaba3fca252
Brahmaji-220671 <n220671@rguktn.ac.in>
2025-12-09 15:35:42 +0530

    Create Inbuilt_functions.js

---
+++ LAB-1/Inbuilt_functions.js
@@ -0,0 +1,25 @@
+//the whole code has been done in console tab
+//alert boxes for notifying the user(dialogue box)
+alert("There is a notification for you");
+//alert using html buttons by clicking them
+<html>
+    <head>
+    </head>
+    <body>
+        <script>
+            function Notification(){
+                alert("there is a notification for you ");
+            }
+        </script>
+        <form>
+            <button type="button" value="Click to get alert" onclick="Notification()">click to get alert</button>
+        </form>
+    </body>
+</html>
+//data taking from user(it is done in console tab) usign prompt function
+let a,b;
+a=prompt("enter the value for a:");
+b=prompt("enter the value for b:");
+console.log(a,b);
+//debugging using console.log()
+
